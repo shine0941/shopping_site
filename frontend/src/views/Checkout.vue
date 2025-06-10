116f5071ae71c6cf2c0317423bb200a32da01730
shine <shine55660000@gmail.com>
2025-06-10 11:31:41 +0800

    using cart item data

--- frontend/src/views/Checkout.vue
+++ frontend/src/views/Checkout.vue
@@ -1,9 +1,6 @@
 <template>
   <v-container style="max-width: 100vw">
-    <v-row>
-      <v-col> Checkout </v-col>
-    </v-row>
-    <template v-if="test_data.length > 0">
+    <template v-if="cart.cartItems.length > 0">
       <v-stepper v-model="stepper" :items="stepper_items" style="width: 50vw" alt-labels>
         <template v-slot:item.1>
           <v-card title="Cart Items" flat>
@@ -19,7 +16,7 @@
                   </tr>
                 </thead>
                 <tbody>
-                  <tr v-for="item in test_data" :key="item.id">
+                  <tr v-for="item in cart.cartItems" :key="item.id">
                     <td>
                       <v-img :src="item.product.images[0].image" style="width: 50px"></v-img>
                     </td>
@@ -79,7 +76,7 @@
                   </tr>
                 </thead>
                 <tbody>
-                  <tr v-for="item in test_data" :key="item.id">
+                  <tr v-for="item in cart.cartItems" :key="item.id">
                     <td>
                       <v-img :src="item.product.images[0].image" style="width: 50px"></v-img>
                     </td>
@@ -114,7 +111,9 @@
     <br />
     <v-row>
       <v-col class="text-end">
-        <!-- <v-btn color="primary" :disabled="stepper != stepper_items.length">Checkout</v-btn> -->
+        <v-btn color="primary" :disabled="stepper != stepper_items.length" @click="checkout"
+          >Checkout</v-btn
+        >
       </v-col>
     </v-row>
   </v-container>
@@ -125,44 +124,6 @@ import { cartStore } from '@/stores/cart'
 import { useUserStore } from '@/stores/user'
 // const user = useUserStore()
 const cart = cartStore()
-const test_data = ref([
-  {
-    id: 11,
-    product: {
-      id: 5,
-      name: 'KODAK 柯達PIXPRO FZ55 5倍光學變焦 數位相機 28mm廣角鏡頭 原廠公司貨',
-      price: '5595.00',
-      images: [
-        {
-          id: 5,
-          image: 'http://192.168.1.100:8001/product_images/product_images/000001_1732845704.webp',
-          order: 0,
-        },
-      ],
-    },
-    subtotal: 11190,
-    quantity: 2,
-    cart: 1,
-  },
-  {
-    id: 12,
-    product: {
-      id: 4,
-      name: 'SONY 索尼DSC-RX100M7G 數位相機 (公司貨)',
-      price: '36460.00',
-      images: [
-        {
-          id: 4,
-          image: 'http://192.168.1.100:8001/product_images/product_images/000001_1746416275.webp',
-          order: 0,
-        },
-      ],
-    },
-    subtotal: 36460,
-    quantity: 1,
-    cart: 1,
-  },
-])
 const shipping = ref(0)
 const shipping_data = ref([
   { name: 'Standard Shipping', price: 60 },
@@ -175,17 +136,20 @@ const showCartItems = () => {
   console.log(JSON.stringify(cart.cartItems))
 }
 const caculateCartItemSubtotal = (item) => {
-  console.log(item.quantity, Number(item.product.price))
   return item.quantity * Number(item.product.price)
 }
 const caculateCartSubtotal = () => {
   let subtotal = 0
-  for (const item of test_data.value) {
-    console.log(item)
+  for (const item of cart.cartItems) {
     subtotal += item.quantity * Number(item.product.price)
   }
 
   return subtotal
 }
+const handleItemChange = async (item) => {
+  console.log('handleItemChange', item)
+  await cart.updateCartItem(item)
+}
+const checkout = () => {}
 onMounted(showCartItems)
 </script>
