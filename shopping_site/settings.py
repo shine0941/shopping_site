3d62c7897126948872474b5cee72321e66176ce8
shine <shine55660000@gmail.com>
2025-09-03 16:24:05 +0800

    CI workflow add docker

--- shopping_site/settings.py
+++ shopping_site/settings.py
@@ -118,7 +118,7 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB', 'shopping_site'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', 'postgres'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': os.getenv('DB_PORT', '5432'),
     }
     # 'default': {
